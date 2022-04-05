bdc23b9edb996da2b90b79ca360cd02f070eb238
Maria <dragunova.maria97@gmail.com>
2022-04-06 00:04:14 +0300

    Added "struct Cat"

--- DZ4_Masha.playground/Contents.swift
+++ DZ4_Masha.playground/Contents.swift
@@ -1,3 +1,66 @@
 import UIKit
 
+// MARK: 1. Написать функцию имеющую аргументы с двойными именами
+// 2. Обработать перечисление значений через for in для словаря и массива
+// 3. Поработать с опциональными значениями с ??
+// 4. Создать свою структуру с двумя полями разного типа и двумя методами разного типа и проинициализировать используя модификаторы доступа
+// 5. Создать массив со своим типом данных и вывести его значения в цикле for in и while
+
+
+struct Cat {
+    private var name: String?
+    var age: Int
+    var nicknames: Set<String>
+    
+    init(name: String?, age: Int, nicknames: Set<String>) {
+        self.name = name
+        self.age = age
+        self.nicknames = nicknames
+    }
+    
+    mutating func callTheCat(calling number: Int, whatFor: String) {
+        self.nicknames.insert(self.name ?? "Котик")
+        var numberIterations = 0
+        
+        for i in self.nicknames {
+            numberIterations += 1
+            print("\(i) приходи \(whatFor)!")
+            
+            if numberIterations >= number {
+                break
+            } 
+        }
+        
+        if number > self.nicknames.count {
+            var repeating = number - self.nicknames.count
+            
+            while repeating > 0 {
+                repeating -= 1
+                print("Кис кис кис")
+            }
+        }
+    }
+    
+    mutating func addTheNickname(_ new: Set<String>) -> Set<String> {
+        self.nicknames = self.nicknames.union(new)
+        return self.nicknames
+    }
+    
+    mutating func happyBirthday() -> Int{
+        self.age += 1
+        print("С Днем Рожденья, \(self.name ?? "Котик")")
+        return self.age
+    }
+}
+
+var myCat = Cat(name: "Моника", age: 2, nicknames: ["Моня", "Китя", "Красавица"])
+
+myCat.callTheCat(calling: 7, whatFor: "кушать")
+myCat.addTheNickname(["Буся"])
+myCat.callTheCat(calling: 3, whatFor: "домой")
+
+var anotherCat = Cat(name: nil, age: 4, nicknames: [])
+anotherCat.callTheCat(calling: 2, whatFor: "кушать")
+
+
 

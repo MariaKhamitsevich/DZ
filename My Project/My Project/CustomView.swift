d7b498a0504f9a9f8a477e5cad859e4cbac35738
Maria <dragunova.maria97@gmail.com>
2022-04-20 14:34:03 +0300

    Finish main screen. Added log in and password text fields

---
+++ My Project/My Project/CustomView.swift
@@ -0,0 +1,12 @@
+//
+//  CustomView.swift
+//  My Project
+//
+//  Created by  Maria Khamitsevich on 20.04.22.
+//
+
+import UIKit
+
+final class CustomView: UIView {
+    
+}
--- My Project/My Project/ViewController.swift
+++ My Project/My Project/ViewController.swift
@@ -7,42 +7,121 @@
 
 import UIKit
 
+// 5. Создать навигационный флоу из трех экранов и навигироваться из кода. Навигация разного типа: презент и пуш
+// 6. Добавить таб-бар на ваш экран и поддержать в нем навигацию.
+
+
 class ViewController: UIViewController {
 
     @IBOutlet var hiddenLabel: UILabel!
     @IBOutlet var confirmButton: UIButton!
     @IBOutlet var loginTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var pencilImage: UIImageView!
+    @IBOutlet var mainSwitch: UISwitch!
     
+    @IBOutlet var credentialsStackView: UIStackView!
+    
+    // MARK: viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         confirmButton.layer.cornerRadius = 8
         loginTextField.placeholder = "My Login"
-        hiddenLabel.text = "Please, enter your login"
+        passwordTextField.placeholder = "Password"
+        hiddenLabel.text = "Please, enter your login!"
+        hiddenLabel.textColor = UIColor(white: 0.5, alpha: 0.5)
+        confirmButton.setTitle("Log In", for: .normal)
         pencilImage.isHidden = false
+        loginTextField.backgroundColor = UIColor(red: 251/251, green: 0, blue: 0, alpha: 0.1)
+        passwordTextField.backgroundColor = UIColor(red: 251/251, green: 0, blue: 0, alpha: 0.1)
+        mainSwitch.isOn = false
         // Do any additional setup after loading the view.
     }
-    
-    @IBAction func makeTouch() {
-        if !hiddenLabel.isHidden && hiddenLabel.text == "Please, enter your login" {
-            hiddenLabel.text = "Please, enter your password"
-            confirmButton.setTitle("Confirm", for: .normal)
-            loginTextField.placeholder = "My Password"
-            
-        } else if !hiddenLabel.isHidden && hiddenLabel.text == "Please, enter your password" {
-            hiddenLabel.isHidden = true
-            confirmButton.setTitle("Deny", for: .normal)
-            loginTextField.placeholder = ""
-            loginTextField.isHidden = true
-            pencilImage.isHidden = true
-        } else {
-            hiddenLabel.isHidden = false
-            hiddenLabel.text = "Please, enter your login"
-            confirmButton.setTitle("Confirm", for: .normal)
-            loginTextField.placeholder = "My Login"
-            loginTextField.isHidden = false
-            pencilImage.isHidden = false
+ 
+    func enableTextFields(_ isEnabled: Bool) {
+        for subview in credentialsStackView.subviews {
+            if let subview = subview as? UITextField {
+                subview.isEnabled = isEnabled
+//                subview.isUserInteractionEnabled = false
+            }
         }
+    }
+// MARK: Enter login in textField
+    //Begin enter login
+    @IBAction func beginEnterLogin(_ sender: UITextField) {
+        enableTextFields(mainSwitch.isOn)
+    }
+    
+    //Continue enter login
+    @IBAction func enterLogin(_ sender: UITextField) {
+        self.loginTextField.becomeFirstResponder()
+        if let text = loginTextField.text {
+        hiddenLabel.text = "Wellcome, \(text)!"
+            hiddenLabel.textColor = UIColor(white: 0, alpha: 1)
+        }
+        passwordTextField.isEnabled = true
+        passwordTextField.text = ""
+        
+        if loginTextField.text == "" {
+            passwordTextField.text = ""
+            passwordTextField.isEnabled = false
+            hiddenLabel.text = "Please, enter your login!"
+            hiddenLabel.textColor = UIColor(white: 0.5, alpha: 0.5)
+        }
+    }
+  
+    //Press return button
+    @IBAction func loginTextFieldTapDown(_ sender: UITextField) {
+        sender.endEditing(true)
+    }
+// MARK: Enter password in textField
+    //Begin enter password
+    @IBAction func beginEnterPassword(_ sender: UITextField) {
+        if loginTextField.text == "" {
+            passwordTextField.isEnabled = false
+        } else {
+            passwordTextField.isEnabled = true
+        }
+    }
+    
+    //Continue enter password
+    @IBAction func enterPassword(_ sender: UITextField) {
+        self.passwordTextField.becomeFirstResponder()
+//        passwordTextField.clearsOnBeginEditing = true
+        if loginTextField.text == "" {
+            passwordTextField.isEnabled = false
+        } else {
+            passwordTextField.isEnabled = true
+        }
+        
+        if var text = passwordTextField.text  {
+            if text.count > 12 {
+                text.removeLast()
+                passwordTextField.text = text
+            }
+        }
+    }
+    //Press return button
+    @IBAction func passwordTextFieldTapDown(_ sender: UITextField) {
+        sender.endEditing(true)
+    }
+// MARK: Switch isOn
+    
+    @IBAction func changeValueSwith(_ sender: UISwitch) {
+        
+        if !sender.isOn {
+            loginTextField.text = ""
+            passwordTextField.text = ""
+        }
+        
+        enableTextFields(sender.isOn)
+    }
+
+    // MARK: Confirm Button Action
+    //Press Confirm button
+    @IBAction func makeTouch() {
+
     }
     
 }

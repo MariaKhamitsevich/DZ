88f505175ecb8605246a00484d3b772231afa42c
Maria <dragunova.maria97@gmail.com>
2022-04-30 20:43:10 +0300

    Moved action in SettingsView

---
+++ ZooBazaar/FavoritesViewController.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritesViewController.swift
+//  ZooBazaar
+//
+//  Created by  Maria Khamitsevich on 27.04.22.
+//
+
+import UIKit
+
+class FavoritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}

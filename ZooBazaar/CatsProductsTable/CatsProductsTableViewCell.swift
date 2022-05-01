c4473cd6917844a03314dc5f995e333288fd44e2
Maria <dragunova.maria97@gmail.com>
2022-05-01 22:06:55 +0300

    Added struct for category of products

---
+++ ZooBazaar/CatsProductsTable/CatsProductsTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  CatsProductsTableViewCell.swift
+//  ZooBazaar
+//
+//  Created by  Maria Khamitsevich on 1.05.22.
+//
+
+import UIKit
+
+struct CategoryOfProducts {
+    let nameOfCategory: String
+    let imageForCategory: UIImage
+}
+
+class CatsProductsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

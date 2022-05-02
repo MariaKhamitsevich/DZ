8a639dadce13c519e85b28f733ee117d739ed71c
Maria Khamitsevich <dragunova.maria97@gmail.com>
2022-05-02 21:23:26 +0300

    set all constraints for table cell

--- ZooBazaar/CatsProductsTable/CatsProductsTableViewCell.swift
+++ ZooBazaar/CatsProductsTable/CatsProductsTableViewCell.swift
@@ -16,7 +16,7 @@ struct CategoryOfProducts {
 
 class CatsProductsTableViewCell: UITableViewCell {
     
-    private lazy var imageForProduct: UIImageView = {
+    private lazy var productImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
         
@@ -25,6 +25,9 @@ class CatsProductsTableViewCell: UITableViewCell {
     
     private lazy var productNameLabel: UILabel = {
         let label = UILabel()
+        label.textColor = UIColor(red: 188/255, green: 22/255, blue: 72/255, alpha: 1)
+        label.font = UIFont.boldSystemFont(ofSize: 16)
+        label.textAlignment = .left
         label.translatesAutoresizingMaskIntoConstraints = false
         
         return label
@@ -32,6 +35,15 @@ class CatsProductsTableViewCell: UITableViewCell {
     
     private lazy var productPriceLabel: UILabel = {
         let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        
+        return label
+    }()
+    
+    private lazy var productDescription: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 4
+        label.textAlignment = .left
         label.translatesAutoresizingMaskIntoConstraints = false
         
         return label
@@ -90,17 +102,203 @@ class CatsProductsTableViewCell: UITableViewCell {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         selectionStyle = .none
         
-        contentView.addSubview(imageForProduct)
+        contentView.addSubview(productImageView)
         contentView.addSubview(productNameLabel)
+        contentView.addSubview(productDescription)
         contentView.addSubview(productPriceLabel)
         contentView.addSubview(productWeightLabels)
+        
+        setAllConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func setAllConstraints() {
+        productImageConstraints()
+        productNameConstraints()
+        productDescriptionConstraints()
+        productPriceConstraints()
+        productWeightConstraints()
+    }
     
+    private func productImageConstraints() {
+        let topConstraint = NSLayoutConstraint(
+            item: productImageView,
+            attribute: .top,
+            relatedBy: .equal,
+            toItem: contentView,
+            attribute: .topMargin,
+            multiplier: 1,
+            constant: 4)
+        
+        let traillingConstraint = NSLayoutConstraint(
+            item: productImageView,
+            attribute: .trailing,
+            relatedBy: .equal,
+            toItem: contentView,
+            attribute: .trailingMargin,
+            multiplier: 1,
+            constant: -4)
+        
+        let bottomConstraint = NSLayoutConstraint(
+            item: productImageView,
+            attribute: .bottom,
+            relatedBy: .equal,
+            toItem: contentView,
+            attribute: .bottom,
+            multiplier: 1,
+            constant: -4)
+        
+        let heighConstraint = NSLayoutConstraint(
+            item: productImageView,
+            attribute: .height,
+            relatedBy: .equal,
+            toItem: .none,
+            attribute: .height,
+            multiplier: 1,
+            constant: 56)
+        
+        let widthConstraint = NSLayoutConstraint(
+            item: productImageView,
+            attribute: .width,
+            relatedBy: .equal,
+            toItem: .none,
+            attribute: .width,
+            multiplier: 1,
+            constant: 56)
+        
+        [topConstraint,
+         bottomConstraint,
+         traillingConstraint,
+         heighConstraint,
+         widthConstraint].forEach({ $0.isActive = true })
+    }
+    
+    private func productNameConstraints() {
+        let topConstraint = NSLayoutConstraint(
+            item: productNameLabel,
+            attribute: .top,
+            relatedBy: .equal,
+            toItem: contentView,
+            attribute: .topMargin,
+            multiplier: 1,
+            constant: 4)
+        
+        let leadingConstraint = NSLayoutConstraint(
+            item: productNameLabel,
+            attribute: .leading,
+            relatedBy: .equal,
+            toItem: contentView,
+            attribute: .leadingMargin,
+            multiplier: 1,
+            constant: 8)
+        
+        let traillingConstraint = NSLayoutConstraint(
+            item: productNameLabel,
+            attribute: .trailing,
+            relatedBy: .equal,
+            toItem: productImageView,
+            attribute: .trailingMargin,
+            multiplier: 1,
+            constant: -8)
+        
+        [topConstraint,
+         traillingConstraint,
+         leadingConstraint].forEach({ $0.isActive = true })
+    }
+    
+    private func productDescriptionConstraints() {
+        let topConstraint = NSLayoutConstraint(
+            item: productDescription,
+            attribute: .top,
+            relatedBy: .equal,
+            toItem: productNameLabel,
+            attribute: .bottom,
+            multiplier: 1,
+            constant: 4)
+        
+        let leadingConstraint = NSLayoutConstraint(
+            item: productDescription,
+            attribute: .leading,
+            relatedBy: .equal,
+            toItem: contentView,
+            attribute: .leadingMargin,
+            multiplier: 1,
+            constant: 8)
+        
+        let traillingConstraint = NSLayoutConstraint(
+            item: productDescription,
+            attribute: .trailing,
+            relatedBy: .equal,
+            toItem: productImageView,
+            attribute: .trailingMargin,
+            multiplier: 1,
+            constant: -8)
+        
+        
+        [topConstraint,
+         traillingConstraint,
+         leadingConstraint].forEach({ $0.isActive = true })
+    }
+    
+    private func productPriceConstraints() {
+        let bottomConstraint = NSLayoutConstraint(
+            item: productPriceLabel,
+            attribute: .bottomMargin,
+            relatedBy: .equal,
+            toItem: contentView,
+            attribute: .bottom,
+            multiplier: 1,
+            constant: -4)
+        
+        let leadingConstraint = NSLayoutConstraint(
+            item: productPriceLabel,
+            attribute: .leading,
+            relatedBy: .equal,
+            toItem: contentView,
+            attribute: .leadingMargin,
+            multiplier: 1,
+            constant: 8)
+        
+        [bottomConstraint,
+         leadingConstraint].forEach({ $0.isActive = true })
+    }
+    
+    private func productWeightConstraints() {
+        let bottomConstraint = NSLayoutConstraint(
+            item: productWeightLabels,
+            attribute: .bottomMargin,
+            relatedBy: .equal,
+            toItem: contentView,
+            attribute: .bottom,
+            multiplier: 1,
+            constant: -4)
+        
+        let leadingConstraint = NSLayoutConstraint(
+            item: productWeightLabels,
+            attribute: .leading,
+            relatedBy: .equal,
+            toItem: productPriceLabel,
+            attribute: .trailing,
+            multiplier: 1,
+            constant: 8)
+        
+        let traillingConstraint = NSLayoutConstraint(
+            item: productWeightLabels,
+            attribute: .trailing,
+            relatedBy: .equal,
+            toItem: productImageView,
+            attribute: .leading,
+            multiplier: 1,
+            constant: -8)
+        
+        
+        [bottomConstraint,
+         traillingConstraint,
+         leadingConstraint].forEach({ $0.isActive = true })
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
